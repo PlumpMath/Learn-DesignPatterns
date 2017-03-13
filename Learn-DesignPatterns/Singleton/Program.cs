d2e583e5ede101cc6713c4604fba2b62016aa5fb
Roman <376489098@qq.com>
2017-03-13 11:45:57 +0800

    单例模式：全局只能有一个实例，

---
+++ Learn-DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //问题：单例类和静态类的区别
+            //1、静态类不能控制其初始化，
+            //2、单例类已创建就不会被GC回收（静态属性变量不会被GC清除）
+            //3、静态类不能继承类也不能实现接口
+            //4、单例类可以实现多态性
+
+            var single1 = Singleton.GetInstance();
+
+            var single2 = Singleton.GetInstance();
+
+            Console.WriteLine(single1 == single2 ? "我是相同的" : "我是不同的");
+
+
+            var singletest1=SingletonTest.GetSingletonTest();
+            var singletest2 = SingletonTest.GetSingletonTest();
+
+            Console.WriteLine(singletest1 == singletest2 ? "我是相同的" : "我是不同的");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Learn-DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// 懒汉式单例模式：使用的时候才实例化
+    /// </summary>
+    public class Singleton
+    {
+        public static Singleton instance = null;
+        //创建一个静态只读的进程辅助对象
+        private static object Obj = new object();
+        /// <summary>
+        /// 私有构造函数，不让外界对Singleton在实例化
+        /// </summary>
+        private Singleton()
+        {
+            
+        }
+
+        /// <summary>
+        /// 用GetInstance方法返回Singleton实例
+        /// </summary>
+        /// <returns></returns>
+        public static Singleton GetInstance()
+        {
+            //先做判断，再进行加锁，锁是为影响性能的
+            if (instance != null) return instance;
+            //lock是保证一个线程位于代码的临界区时，另一个线程不进入临界区。
+            //如果其他线程试图进入锁定的代码，它将一直等待（线程就阻塞了），知道对象释放。
+            lock (Obj)
+            {
+                if (instance == null)
+                {
+                    //这里一定是把新实例赋值给instance，不能直接返回new Singleton()，本人就是错在这里了。呜呜呜
+                    instance = new Singleton();
+
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    /// <summary>
+    /// 饿汉式单利模式：再程序一运行就实例化
+    /// </summary>
+    public sealed class SingletonTest
+    {
+        private static readonly SingletonTest test = new SingletonTest();
+
+        private SingletonTest()
+        {
+            
+        }
+
+        public static SingletonTest GetSingletonTest()
+        {
+            return test;
+        }
+    }
+}
